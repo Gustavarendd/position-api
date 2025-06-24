80693eeaf2359907a748b15fc25a182596200396
Gustav Arend <gustavarend@me.com>
2025-06-24 10:40:57 +0200

    Update CORS middleware to use regex for allowed origins

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=r"https://.*\.amphitrite\.fr|http://localhost:5173",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
